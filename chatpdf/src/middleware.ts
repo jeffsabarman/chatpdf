31772e48eaad618566acf9a2dfc903f67cc90a01
Jeffrey Triandi <jeffsabarman@gmail.com>
2024-06-13 21:45:41 +0700

    setup clerk and drizzle orm

---
+++ chatpdf/src/middleware.ts
@@ -0,0 +1,19 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  //   "/",
+  // '/dashboard(.*)',
+  // '/forum(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+export const config = {
+  matcher: [
+    "/((?!.*\\..*|_next).*)", // Don't run middleware on static files
+    "/", // Run middleware on index page
+    "/(api|trpc)(.*)", // Run middleware on API routes
+  ],
+};
